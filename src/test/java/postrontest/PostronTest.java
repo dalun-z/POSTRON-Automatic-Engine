346cb91271594e7503dc5f3991481e909a02cb73
build a home <98255460+dalun-z@users.noreply.github.com>
2023-07-27 17:23:43 -0700

    Update PostronTest.java

--- src/test/java/postrontest/PostronTest.java
+++ src/test/java/postrontest/PostronTest.java
@@ -10,7 +10,9 @@ public class PostronTest {
 			AndroidDriver driver = AndroidDriverFactory.getDriverInstance();
 			
 			TestScenarios.CashierLogin();
-			
+			TestScenarios.DineIn();
+			TestScenarios.SearchTab();
+			TestScenarios.MenuTest();
 		} catch (Exception e) {
 			System.out.println(e.getCause());
 			System.out.println(e.getMessage());
